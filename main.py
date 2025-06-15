486358f034b3201a48651f2e47937404708edaf8
RaihanKabir277 <raihank0198@gmail.com>
2025-06-15 21:53:58 +0600

    resolve the problem and add

--- main.py
+++ main.py
@@ -27,36 +27,26 @@ import random
 timmy = t.Turtle()
 t.colormode(255)
 
-color_list = [(252, 251, 248), (219, 173, 124), (159, 180, 190), (134, 73, 53), (49, 102, 153), (245, 232, 236), (118, 82, 93), (179, 140, 150), (247, 251, 248), (41, 46, 65), 
+color_list = [(219, 173, 124), (159, 180, 190), (134, 73, 53), (49, 102, 153),(118, 82, 93), (179, 140, 150),(41, 46, 65), 
 (162, 104, 151), (126, 173, 114), (83, 96, 183), (67, 9, 27), (238, 241, 245), (81, 133, 107), (231, 188, 138), (52, 62, 79), (195, 90, 72), (116, 43, 58), (60, 41, 28), (92, 144, 117), (70, 67, 52), (182, 187, 207), (211, 181, 189), (102, 51, 38), (174, 199, 203), (181, 201, 180), (210, 184, 180), (41, 73, 82)]
 
-def x_wise():
-    for _ in range(10):
-        timmy.dot(20, random.choice(color_list))
-        timmy.penup()
+timmy.speed("fastest")
+timmy.penup()
+timmy.hideturtle()
+timmy.setheading(220)
+timmy.forward(300)
+timmy.setheading(0)
+
+for dot_counts in range(1, 100 + 1):
+    timmy.dot(20, random.choice(color_list))
+    timmy.forward(50)
+    if dot_counts % 10 == 0:
+        timmy.setheading(90)
         timmy.forward(50)
-        timmy.pendown()
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
 
-def left_wise():
-    timmy.left(90)
-    timmy.penup()
-    timmy.forward(50)
-    timmy.pendown()
-    timmy.left(90)
-
-def right_wise():
-    timmy.right(90)
-    timmy.penup()
-    timmy.forward(50)
-    timmy.pendown()
-    timmy.right(90)
-
-
-for _ in range(10):
-    x_wise()
-    left_wise()
-    x_wise()
-    right_wise()
 
     
 
@@ -64,3 +54,37 @@ for _ in range(10):
 
 screen = t.Screen()
 screen.exitonclick()
+
+
+
+# Below code has a error its run for in a y cordinate 20 times , and other problem aslo -----
+
+
+# def x_wise():
+#     for _ in range(10):
+#         timmy.dot(20, random.choice(color_list))
+#         timmy.penup()
+#         timmy.forward(50)
+#         timmy.pendown()
+
+# def left_wise():
+#     timmy.left(90)
+#     timmy.penup()
+#     timmy.forward(50)
+#     timmy.pendown()
+#     timmy.left(90)
+
+# def right_wise():
+#     timmy.right(90)
+#     timmy.penup()
+#     timmy.forward(50)
+#     timmy.pendown()
+#     timmy.right(90)
+
+
+# for _ in range(10):
+#     # x_wise()
+#     left_wise()
+#     x_wise()
+#     right_wise()
+
